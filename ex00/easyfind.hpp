a3de89e8f08568dbcdf02be7bc769c884c3c2678
skitheom <SahoKitahara@my.uopeople.edu>
2025-01-21 19:52:54 +0900

    update test cases

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 03:41:40 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/21 05:06:52 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:49:40 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,38 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
+#include <iostream>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 
 template <typename T> typename T::iterator easyfind(T &t, int n) {
   typename T::iterator it = std::find(t.begin(), t.end(), n);
   if (it == t.end()) {
-    throw std::out_of_range("Value not found");
+    std::ostringstream oss;
+    oss << "Value " << n << " not found";
+    throw std::out_of_range(oss.str());
   }
   return it;
+}
+
+template <typename T> void testEasyFind(T &t, const std::string &type, int n) {
+  std::cout << "Searching " << n << " in " << type << " ... ";
+  try {
+    std::cout << *(easyfind(t, n)) << " is found! :D" << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+template <typename T>
+void printContainer(const T &container, const std::string &type) {
+  std::cout << "Values in " << type << " : ";
+  for (typename T::const_iterator it = container.begin(); it != container.end();
+       ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
 }
 
 #endif // EASYFIND_HPP
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,70 +6,54 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 04:29:29 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/21 04:29:30 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:52:02 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <deque>
 #include <iostream>
 #include <list>
-#include <set>
 #include <vector>
 
-void testVector() {
-  std::cout << "=== Vector ===" << std::endl;
-  try {
-    std::vector<int> v;
-    v.push_back(0);
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
+static const int MAX_CONTAINER_SIZE = 5;
 
-    std::vector<int>::iterator it = easyfind(v, 3);
-    std::cout << "Found: " << *it << " in Vector" << std::endl;
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
+void testVector() {
+  std::cout << "\n=== Testing Vector ===" << std::endl;
+  std::vector<int> v;
+  for (int i = 0; i < MAX_CONTAINER_SIZE; i++) {
+    v.push_back(i);
   }
+  printContainer(v, "vector");
+  testEasyFind(v, "vector", 1);
+  testEasyFind(v, "vector", 7);
+}
+
+void testDeque() {
+  std::cout << "\n=== Testing Deque ===" << std::endl;
+  std::deque<int> d;
+  for (int i = 0; i < MAX_CONTAINER_SIZE; i++) {
+    d.push_back(i * 2);
+  }
+  printContainer(d, "deque");
+  testEasyFind(d, "deque", 0);
+  testEasyFind(d, "deque", 7);
 }
 
 void testList() {
-  std::cout << "=== List ===" << std::endl;
-  try {
-    std::list<int> lst;
-    lst.push_back(0);
-    lst.push_back(2);
-    lst.push_back(4);
-    lst.push_back(8);
-    lst.push_back(16);
-
-    std::list<int>::iterator it = easyfind(lst, 32);
-    std::cout << "Found: " << *it << " in List" << std::endl;
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
+  std::cout << "\n=== Testing List ===" << std::endl;
+  std::list<int> lst;
+  for (int i = 0; i < MAX_CONTAINER_SIZE; i++) {
+    lst.push_back(i * 3);
   }
-}
-
-void testSet() {
-  std::cout << "=== Set ===" << std::endl;
-  try {
-    std::set<int> s;
-    s.insert(0);
-    s.insert(3);
-    s.insert(6);
-    s.insert(9);
-    s.insert(12);
-
-    std::set<int>::iterator it = easyfind(s, 12);
-    std::cout << "Found: " << *it << " in Set" << std::endl;
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-  }
+  printContainer(lst, "list");
+  testEasyFind(lst, "list", 3);
+  testEasyFind(lst, "list", 7);
 }
 
 int main() {
   testVector();
+  testDeque();
   testList();
-  testSet();
   return 0;
 }

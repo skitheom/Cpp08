2819daae8935eff6b4517b8cb996551feed6a4b9
skitheom <SahoKitahara@my.uopeople.edu>
2025-01-27 18:52:20 +0900

    ok

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:58:13 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/26 01:32:06 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/01/27 18:31:12 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 #define MUTANTSTACK_HPP
 
 #include <deque>
+#include <iostream>
 #include <stack>
-template <class T> class MutantStack : public std::stack<T> {
-public:
-  MutantStack() : std::stack<T>() {}
-  MutantStack(const MutantStack<T> &other) : std::stack<T>(other) {}
-  ~MutantStack() {}
+#include <string>
 
-  MutantStack<T> &operator=(const MutantStack<T> &other) {
-    if (this != other) {
+template <typename T> class MutantStack : public std::stack<T> {
+public:
+  MutantStack() : std::stack<T>() { printDebugMsg(DEFAULT_CONSTRUCTOR); }
+
+  MutantStack(const MutantStack &other) : std::stack<T>(other) {
+    printDebugMsg(COPY_CONSTRUCTOR);
+  }
+
+  ~MutantStack() { printDebugMsg(DESTRUCTOR); }
+
+  MutantStack &operator=(const MutantStack &other) {
+    printDebugMsg(ASSIGNMENT_OPERATOR);
+    if (this != &other) {
       std::stack<T>::operator=(other);
     }
     return *this;
@@ -33,15 +41,75 @@ public:
   typedef typename std::deque<T>::reverse_iterator reverse_iterator;
   typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
 
-  iterator begin() { return this->c.begin(); }
-  iterator end() { return this->c.end(); }
-  const_iterator begin() const { return this->c.begin(); }
-  const_iterator end() const { return this->c.end(); }
+  iterator begin() {
+    printDebugMsg(BEGIN_NON_CONST);
+    return this->c.begin();
+  }
 
-  reverse_iterator rbegin() { return this->c.rbegin(); }
-  reverse_iterator rend() { return this->c.rend(); }
-  const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-  const_reverse_iterator rend() const { return this->c.rend(); }
+  iterator end() {
+    printDebugMsg(END_NON_CONST);
+    return this->c.end();
+  }
+
+  const_iterator begin() const {
+    printDebugMsg(BEGIN_CONST);
+    return this->c.begin();
+  }
+
+  const_iterator end() const {
+    printDebugMsg(END_CONST);
+    return this->c.end();
+  }
+
+  reverse_iterator rbegin() {
+    printDebugMsg(RBEGIN_NON_CONST);
+    return this->c.rbegin();
+  }
+
+  reverse_iterator rend() {
+    printDebugMsg(REND_NON_CONST);
+    return this->c.rend();
+  }
+
+  const_reverse_iterator rbegin() const {
+    printDebugMsg(RBEGIN_CONST);
+    return this->c.rbegin();
+  }
+
+  const_reverse_iterator rend() const {
+    printDebugMsg(REND_CONST);
+    return this->c.rend();
+  }
+
+private:
+  enum DebugMsgType {
+    DEFAULT_CONSTRUCTOR,
+    COPY_CONSTRUCTOR,
+    DESTRUCTOR,
+    ASSIGNMENT_OPERATOR,
+    BEGIN_NON_CONST,
+    END_NON_CONST,
+    BEGIN_CONST,
+    END_CONST,
+    RBEGIN_NON_CONST,
+    REND_NON_CONST,
+    RBEGIN_CONST,
+    REND_CONST
+  };
+
+  void printDebugMsg(MutantStack<T>::DebugMsgType msgType) const {
+#ifdef DISPLAY_DEBUG_MSG
+    static const char *messages[] = {
+        "Default constructor called",  "Copy constructor called",
+        "Destructor called",           "Assignment operator called",
+        "begin() called (non-const)",  "end() called (non-const)",
+        "begin() called (const)",      "end() called (const)",
+        "rbegin() called (non-const)", "rend() called (non-const)",
+        "rbegin() called (const)",     "rend() called (const)"};
+    std::cerr << "[DEBUG] " << messages[msgType] << std::endl;
+#endif
+    (void)msgType;
+  }
 };
 
 #endif // MUTANTSTACK_HPP
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,30 +6,43 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:57:53 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/26 01:07:41 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/01/27 18:50:17 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <deque>
 #include <iostream>
+#include <list>
+#include <stack>
+#include <vector>
 
 void sampleTest() {
   std::cout << "\n=== Sample Test ===" << std::endl;
+
   MutantStack<int> mstack;
+
   mstack.push(5);
   mstack.push(17);
+
   std::cout << mstack.top() << std::endl;
+
   mstack.pop();
+
   std::cout << mstack.size() << std::endl;
+
   mstack.push(3);
   mstack.push(5);
   mstack.push(737);
   //[...]
   mstack.push(0);
+
   MutantStack<int>::iterator it = mstack.begin();
   MutantStack<int>::iterator ite = mstack.end();
+
   ++it;
   --it;
+
   while (it != ite) {
     std::cout << *it << std::endl;
     ++it;
@@ -37,80 +50,138 @@ void sampleTest() {
   std::stack<int> s(mstack);
 }
 
-void edgeCaseTest() {
-  std::cout << "\n=== Edge Case Test ===" << std::endl;
-  ;
-  // TODO: later to write here
+void emptyStackTest(bool enableSegFault = false) {
+  std::cout << "\n=== Empty Stack Test ===" << std::endl;
+  MutantStack<int> emptyMutantStack;
+  std::stack<int> emptyStack;
+
+  if (!enableSegFault) {
+    std::cout << "Avoiding Segmentation fault by return;\n"
+              << "Call 'emptyStackTest(true);' to test top() on empty stack"
+              << std::endl;
+    return;
+  }
+  std::cout << "Trying top() on empty mutant stack:" << std::endl;
+  emptyMutantStack.top();
+  std::cout << "Trying top() on empty stack:" << std::endl;
+  emptyStack.top();
 }
 
-void testLargeData() {
-  std::cout << "\n=== Test: Large Data ===" << std::endl;
-  MutantStack<int> mstack;
-  const int largeSize = 10000;
+void largeDataTest() {
 
+  std::cout << "\n=== Large Data Test  ===" << std::endl;
+
+  MutantStack<int> mstack;
+  const int largeSize = 100000;
+
+  std::cout << "Trying to push() " << largeSize << " elements..." << std::endl;
   for (int i = 0; i < largeSize; ++i) {
     mstack.push(i);
   }
+  std::cout << "Size of MutantStack: " << mstack.size() << std::endl;
 
-  std::cout << "Size after pushing " << largeSize
-            << " elements: " << mstack.size() << std::endl;
-
+  std::cout << "Trying to pop() " << largeSize << " elements..." << std::endl;
   for (int i = 0; i < largeSize; ++i) {
     mstack.pop();
   }
-
-  std::cout << "Size after popping all elements: " << mstack.size()
-            << std::endl;
+  std::cout << "Size of MutantStack: " << mstack.size() << std::endl;
 }
 
-void testReverseIterator() {
-  std::cout << "\n=== Test: Reverse Iterator ===" << std::endl;
-  MutantStack<int> mstack;
-  mstack.push(1);
-  mstack.push(2);
-  mstack.push(3);
-  mstack.push(4);
+void nonConstIteratorTest() {
+  std::cout << "\n=== Non Const Iterator Test ===" << std::endl;
 
-  std::cout << "Contents in reverse order:" << std::endl;
+  MutantStack<int> mstack;
+  const int size = 10;
+
+  for (size_t i = 0; i < size; i++) {
+    mstack.push(i);
+  }
+
+  std::cout << "Contents in regular order:";
+  for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end();
+       ++it) {
+    std::cout << " " << *it;
+  }
+  std::cout << "\nContents in reverse order:";
   for (MutantStack<int>::reverse_iterator rit = mstack.rbegin();
        rit != mstack.rend(); ++rit) {
-    std::cout << *rit << std::endl;
+    std::cout << " " << *rit;
   }
+  std::cout << std::endl;
 }
 
-/*
-void testCustomContainer() {
-  std::cout << "\n=== Test: Custom Container ===" << std::endl;
-  MutantStack<int, std::vector<int>> mstack; // std::vector を使用
-  mstack.push(42);
-  mstack.push(84);
+void constIteratorTest() {
+  std::cout << "\n=== Const Iterator Test ===" << std::endl;
 
-  for (MutantStack<int, std::vector<int>>::iterator it = mstack.begin(); it !=
-mstack.end(); ++it) { std::cout << *it << std::endl;
+  MutantStack<int> tmpStack;
+  const int size = 10;
+
+  for (size_t i = 0; i < size; i++) {
+    tmpStack.push(i);
   }
-}
- */
 
-void testStdStackCompatibility() {
-  std::cout << "\n=== Test: std::stack Compatibility ===" << std::endl;
+  const MutantStack<int> cmStack = tmpStack;
+
+  std::cout << "Contents in regular order:";
+  for (MutantStack<int>::const_iterator cit = cmStack.begin();
+       cit != cmStack.end(); ++cit) {
+    std::cout << " " << *cit;
+  }
+
+  std::cout << "\nContents in reverse order:";
+  for (MutantStack<int>::const_reverse_iterator rcit = cmStack.rbegin();
+       rcit != cmStack.rend(); ++rcit) {
+    std::cout << " " << *rcit;
+  }
+  std::cout << std::endl;
+}
+
+void compatibilityTest() {
+  std::cout << "\n=== Compatibility Test ===" << std::endl;
+
   MutantStack<int> mstack;
-  mstack.push(10);
-  mstack.push(20);
+  const int size = 4;
 
-  std::stack<int> s(mstack);
+  for (size_t i = 0; i < size; i++) {
+    mstack.push(i);
+  }
 
-  std::cout << "Contents of std::stack:" << std::endl;
-  while (!s.empty()) {
-    std::cout << s.top() << std::endl;
-    s.pop();
+  std::vector<int> v;
+  std::deque<int> d;
+  std::list<int> lst;
+  std::stack<int> stk;
+
+  for (size_t i = 0; i < size; i++) {
+    v.push_back(i);
+    d.push_back(i);
+    lst.push_back(i);
+    stk.push(i);
+  }
+
+  for (size_t i = 0; i < size; i++) {
+    std::cout << "MutantStack: " << mstack.top() << ", ";
+    mstack.pop();
+
+    std::cout << "std::vector: " << v.back() << ", ";
+    v.pop_back();
+
+    std::cout << "std::deque: " << d.back() << ", ";
+    d.pop_back();
+
+    std::cout << "std::list:" << lst.back() << ", ";
+    lst.pop_back();
+
+    std::cout << "std::stack: " << stk.top() << std::endl;
+    stk.pop();
   }
 }
 
 int main() {
   sampleTest();
-  edgeCaseTest();
-  testLargeData();
-  testReverseIterator();
-  testStdStackCompatibility();
+  emptyStackTest(); // emptyStackTest(true); <- for testing
+  largeDataTest();
+  nonConstIteratorTest();
+  constIteratorTest();
+  compatibilityTest();
   return 0;
 }

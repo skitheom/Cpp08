567ae4f1646648c18e82145489e37afdc273856e
skitheom <SahoKitahara@my.uopeople.edu>
2025-03-05 18:36:43 +0900

    refactor: test改善

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,70 +6,39 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 04:29:29 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/25 02:03:34 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/03/05 18:25:53 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include "test.hpp"
+#include <cstdlib>
+#include <ctime>
 #include <deque>
 #include <iostream>
 #include <list>
 #include <vector>
 
-static const int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-void testVector() {
-  std::vector<int> v;
-  setupContainer(v);
-  displayInfo(v, "vector");
-  testEasyFind(v, 0);
-  testEasyFind(v, -1);
-}
-
-void testDeque() {
-  std::deque<int> d;
-  setupContainer(d);
-  displayInfo(d, "deque");
-  testEasyFind(d, 1);
-  testEasyFind(d, 11);
-}
-
-void testList() {
-  std::list<int> lst;
-  setupContainer(lst);
-  displayInfo(lst, "list");
-  testEasyFind(lst, 2);
-  testEasyFind(lst, 13);
-}
-
-void testConstVector() {
-  const std::vector<int> v(arr, arr + sizeof(arr) / sizeof(arr[0]));
-  displayInfo(v, "const vector");
-  testEasyFind(v, 3);
-  testEasyFind(v, 17);
-}
-
-void testConstDeque() {
-  const std::deque<int> d(arr, arr + sizeof(arr) / sizeof(arr[0]));
-  displayInfo(d, "const deque");
-  testEasyFind(d, 5);
-  testEasyFind(d, 19);
-}
-
-void testConstList() {
-  const std::list<int> lst(arr, arr + sizeof(arr) / sizeof(arr[0]));
-  displayInfo(lst, "const list");
-  testEasyFind(lst, 7);
-  testEasyFind(lst, 23);
-}
-
 int main() {
-  testVector();
-  testDeque();
-  testList();
-  testConstVector();
-  testConstDeque();
-  testConstList();
+  const int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::srand(std::time(0));
+
+  std::vector<int> v;
+  testContainer(v, "vector");
+
+  std::deque<int> d;
+  testContainer(d, "deque");
+
+  std::list<int> lst;
+  testContainer(lst, "list");
+
+  const std::vector<int> cv(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  testContainer(cv, "const vector");
+
+  const std::deque<int> cd(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  testContainer(cd, "const deque");
+
+  const std::list<int> clst(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  testContainer(clst, "const list");
   return 0;
 }
--- ex00/test.hpp
+++ ex00/test.hpp
@@ -6,13 +6,14 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:23:27 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/25 02:07:24 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/03/05 18:34:52 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_HPP
 #define TEST_HPP
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
@@ -42,7 +43,7 @@ template <typename T> void displayInfo(const T &t, const std::string &type) {
 }
 
 template <typename T> void testEasyFind(T &t, int n) {
-  std::cout << "Searching " << n << "...\t";
+  std::cout << "Searching " << n << "\t->\t";
   try {
     std::cout << *(easyfind(t, n)) << " is found! :D" << std::endl;
   } catch (const std::exception &e) {
@@ -51,12 +52,27 @@ template <typename T> void testEasyFind(T &t, int n) {
 }
 
 template <typename T> void testEasyFind(const T &t, int n) {
-  std::cout << "Searching " << n << "...\t";
+  std::cout << "Searching " << n << "\t->\t";
   try {
     std::cout << *(easyfind(t, n)) << " is found! :D" << std::endl;
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
+}
+
+template <typename T> void testContainer(T &t, const std::string &type) {
+  setupContainer(t);
+  displayInfo(t, type);
+  testEasyFind(t, rand() % 10);
+  testEasyFind(t, rand() % 99);
+  testEasyFind(t, -1);
+}
+
+template <typename T> void testContainer(const T &t, const std::string &type) {
+  displayInfo(t, type);
+  testEasyFind(t, rand() % 10);
+  testEasyFind(t, rand() % 99);
+  testEasyFind(t, -1);
 }
 
 #endif // TEST_HPP
